7f85e3d126104d1580ccc78c7b926a121449aa4b
daniellluis <daniellluis@gmail.com>
2023-03-20 20:19:55 +0100

    probando git dia 1

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,15 @@ const App = () => {
                             <CrossIcon />
                         </button>
                     </article>
+                    <article className="flex gap-4 border-b border-b-gray-400">
+                        <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
+                        <p className="grow text-gray-600">
+                            Complete Online JS curse
+                        </p>
+                        <button className="flex-none">
+                            <CrossIcon />
+                        </button>
+                    </article>
 
                     <article className="flex gap-4 border-b border-b-gray-400">
                         <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
